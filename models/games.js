de9f2053357955619e9fad0e1a74e564b3b3f868
Jack <jackpmalone01@gmail.comgit config --global user.name jackMalone2001>
2022-04-12 22:14:16 +0100

    added image stuff

--- models/games.js
+++ models/games.js
@@ -3,9 +3,10 @@ const joi = require('joi');
 
 const gameSchema = new mongoose.Schema({
     name: String,
-    developers: [String],
-    publishers: [String],
-    description: String
+    developers: String,
+    publishers: String,
+    description: String,
+    img: String
 });
 
 const Game = mongoose.model('Game', gameSchema);
@@ -15,7 +16,8 @@ function validation(game) {
         name: joi.string().max(50).required(),
         developers: joi.array().items(joi.string()).min(1).max(50).required(),
         publishers: joi.array().items(joi.string()).min(1).max(50).required(),
-        description: joi.string().max(500).required()
+        description: joi.string().max(500).required(),
+        img: joi.string().required()
     });
 
     return schema.validate(game);
